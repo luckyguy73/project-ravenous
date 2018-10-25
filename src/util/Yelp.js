8450c8f15c18b9348de19488438b2f4cdb95a277
Ian De Bie <luckyguy73@yahoo.com>
2018-10-25 11:17:19 -0700

    updated url to solve cors problem
    used heroku cors anywhere url

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,7 +1,7 @@
 const apiKey = 'rmuMfQkif5Ff4EW96K_VCdmBnXKwIWYQRrGsecB0JgLrjN04xHIR9yYFKQ6T2Ff5TTw7-yDgdDvzIwrv5uX-eIjQevHCAcI43c1Gx-H4UV43ADbOf2gNEITPOrSkWnYx';
 export const Yelp = {
   search(term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+    return fetch(`https://infinite-forest-26068.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
       headers: {
         Authorization: `Bearer ${apiKey}`
       }
